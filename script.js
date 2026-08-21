56b1e72af8b7d87cf009ea69297969d613dce6ab
Githubber2662 <95174200+Githubber2662@users.noreply.github.com>
2026-08-21 14:59:13 +0300

    Updated script.js

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
 class Game {
     constructor() {
         this.points = new MetaNum(1);
-        this.dimensions = [[new MetaNum(1), new MetaNum(1), new MetaNum(0)]];
+        this.multiplier = new MetaNum(1);
+        this.dimensions = [[[new MetaNum(1), new MetaNum(1), new MetaNum(0)]]];
+        this.layers = [new MetaNum(1)];
         this.upgrades = {};
         this.challenges = {};
         this.initializeUpgrades();
         this.initializeChallenges();
+        this.initializeAchievements();
     initializeUpgrades() {
-    this.upgrades = {
-      superdimension: {
-        id: 'superdimension',
-        name: "Superdimension",
-        description: "All dimensions are more powerful",
-        boost: new MetaNum(2),
-        baseCost: new MetaNum("F100"),
-        cost: [new MetaNum(2), "mult-superlog"],
-        owned: new MetaNum(0),
-        type: 'power'
-      },
-      fastPacing: {
-        id: 'fastPacing',
-        name: "Fast Pacing",
-        description: "×1.5 passive multiplier",
-        multiplier: new MetaNum(1.5),
-        baseCost: new MetaNum(100),
-        cost: new MetaNum(100),
-        owned: new MetaNum(0),
-        type: 'passive'
-      },
-      exponentialGrowth: {
-        id: 'exponentialGrowth',
-        name: "Exponential Growth",
-        description: "passive multiplier^1.2",
-        multiplier: new MetaNum(1.2),
-        baseCost: new MetaNum(1000),
-        cost: new MetaNum(1000),
-        owned: new MetaNum(0),
-        type: 'passive',
-        isExponential: true
-      }
+        this.upgrades = {
+        };
     };
+    initializeAchievements() {
+        this.achievements = {
+            1: {
+                id: 1,
+                name: "You gotta start somewhere",
+                description: "Buy a dimension",
+                unlocked: false,
+                can: Game.points.gt(1),
+            },
+            2: {
+                id: 2,
+                name: "Monologue",
+                description: "Get 10 points",
+                unlocked: false,
+                can: Game.points.gte(10)
+                
+            }
+            }
+        }
     };
 };
+}
